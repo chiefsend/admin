b1e9dbe47008d6f4dc8c01ff99f59fbe429fc1ed
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-13 12:58:21 +0200

    :sparkles:  add navigation drawer

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-        v-model="drawer"
-        app
-    >
-      Menü
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            ChiefSend
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Admin
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item v-for="item in nav_items" :key="item.title" link :to="item.route">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
     </v-navigation-drawer>
 
     <v-app-bar app dark>
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-btn to="/shares" text>
-        Shares
-      </v-btn>
+      <v-toolbar-title>ChiefSend</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+      <v-avatar color="indigo">
+        <v-icon dark>mdi-account-circle</v-icon>
+      </v-avatar>
     </v-app-bar>
 
     <v-main>
@@ -25,6 +47,15 @@
 <script>
 export default {
   name: 'App',
-  data: () => ({drawer: null}),
+  data() {
+    return {
+      nav_items: [
+        {title: 'Dashboard', icon: 'mdi-view-dashboard', route: '/'},
+        {title: 'Shares', icon: 'mdi-cloud-upload', route: '/shares'},
+      ],
+      right: null,
+      drawer: null
+    }
+  },
 };
 </script>
