ab4039e95d482695bee35b871c3790a26ac7a10b
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-19 15:40:06 +0200

    :bug: fix env variables
    they need the VUE_APP_ prefix

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const ax = axios.create({
-    baseURL: process.env.API || "http://localhost:6969",
+    baseURL: process.env.VUE_APP_API || "http://localhost:6969",
     headers: {
         "Content-type": "application/json",
-        'Authorization': `Bearer ${ btoa(process.env.ADMIN_KEY) }`
+        'Authorization': `Bearer ${btoa(process.env.VUE_APP_ADMIN_KEY)}`
     },
 });
 
