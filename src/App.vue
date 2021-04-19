bbdf8b619a488bcc59e2de35e3c2e0595256321a
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-19 16:03:35 +0200

    :lipstick: :sparkles: Menu on admin account

--- src/App.vue
+++ src/App.vue
@@ -27,13 +27,29 @@
     </v-navigation-drawer>
 
     <v-app-bar app dark>
+      <!-- Left -->
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>ChiefSend</v-toolbar-title>
       <v-spacer></v-spacer>
+      <!-- Right -->
+      <v-menu open-on-hover top offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-avatar color="indigo" v-bind="attrs" v-on="on">
+            <v-icon dark>mdi-account-circle</v-icon>
+          </v-avatar>
+        </template>
+        <v-list>
+          <v-list-item v-for="(item, index) in acc_items" :key="index">
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
 
-      <v-avatar color="indigo">
-        <v-icon dark>mdi-account-circle</v-icon>
-      </v-avatar>
     </v-app-bar>
 
     <v-main>
@@ -50,9 +66,13 @@ export default {
       nav_items: [
         {title: 'Dashboard', icon: 'mdi-view-dashboard', route: '/'},
         {title: 'Shares', icon: 'mdi-cloud-upload', route: '/shares'},
+        {title: 'Jobs', icon: 'mdi-application-cog', route: '/jobs'},
       ],
-      right: null,
-      drawer: null
+      drawer: null,
+      acc_items: [
+        {title: 'Settings',icon: 'mdi-account-settings'},
+        {title: 'Logout', icon: 'mdi-logout'},
+      ]
     }
   },
 };
