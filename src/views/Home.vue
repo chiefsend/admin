4cd26d0d28dc2d0f30545308559e4c34252f19c9
Lukas Böhm <boehm-lukas@outlook.de>
2021-04-19 15:50:05 +0200

    :sparkles:  Start Dashboard

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,56 @@
 <template>
   <v-container>
-    Landing
+    <v-row>
+      <v-col>
+        <v-card color="pink" dark>
+          <v-card-title class="text-h5">
+            Total amount of shares
+          </v-card-title>
+          <v-card-subtitle>
+            {{ stats.number_of_shares }}
+          </v-card-subtitle>
+          <v-card-actions>
+            <v-btn text to="/shares">
+              Show Shares
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col>
+        <v-card color="blue" dark>
+          <v-card-title class="text-h5">
+            Total attachment size
+          </v-card-title>
+          <v-card-subtitle>
+            x attachments: 4356 MB
+          </v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      heatmap
+    </v-row>
   </v-container>
 </template>
 
 <script>
+import ax from "@/api";
+
 export default {
   name: 'Home',
+  data() {
+    return {
+      stats: {}
+    }
+  },
+  mounted() {
+    ax.get(`/shares/stats`)
+        .then((response) => {
+          this.stats = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+  }
 }
 </script>
